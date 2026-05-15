b5f1deb47b6f998fbd268afe900b468f63c3397c
archit0903 <architarora39@gmail.com>
2026-05-15 16:29:31 -0400

    added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Playfair_Display, Lato } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
